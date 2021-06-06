b3fc6bfb9d554427ffa091784b35e42888e1ad7a
pygamer0 <maanasyt@gmail.com>
2021-06-06 11:15:01 +0530

    Add more

--- lua/lush_theme/darc.lua
+++ lua/lush_theme/darc.lua
@@ -101,7 +101,7 @@ local theme = lush(function()
     -- QuickFixLine { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
     Search       { gui = "underline" }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
     -- SpecialKey   { }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
-    -- SpellBad     { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise. 
+    -- SpellBad     { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
     -- SpellCap     { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
     -- SpellLocal   { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
     -- SpellRare    { }, -- Word that is recognized by the spellchecker as one that is hardly ever used.  |spell| Combined with the highlighting used otherwise.
@@ -262,8 +262,10 @@ local theme = lush(function()
     -- TSURI                { };    -- Any URI like a link or email.
     --
     -- Other highlights
-
-    MinimapCurrentLine { fg = hsl("#50fa7b") },
+    MinimapCurrentLine { fg = hsl("#88ff88") }, -- wfxr/minimap.vim
+    GitSignsAdd { bg = hsl("#363636"), fg = hsl("#88ff88") },    -- lewis6991/gitsigns.nvim
+    GitSignsChange { bg = hsl("#363636"), fg = hsl("#ffdd88") },    -- lewis6991/gitsigns.nvim
+    GitSignsDelete { bg = hsl("#363636"), fg = hsl("#ff8888") },    -- lewis6991/gitsigns.nvim
   }
 end)
 
